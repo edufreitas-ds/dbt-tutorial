a57e58dc2fe6f5a911e0fb8450073e68da045098
edufreitas-ds <freitas.eduardoantonio@gmail.com>
2021-09-23 18:10:29 -0300

    "[add]stg_customer

--- models/customers.sql
+++ models/customers.sql
@@ -5,12 +5,7 @@
 }}
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
